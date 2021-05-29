3aaf5ddc5dd9f4df883b393b3a232c4685fa6792
sanneappelman <84176938+sanneappelman@users.noreply.github.com>
2021-05-29 20:00:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Hallo Winc Academy studenten")
+//dit is een grote som
+/*let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication);
+let division = 637263 / 54;
+console.log("division:",division);*/
+
+console.log("einde van de opdracht")
+
+//De code die als comment wordt aangegeven verschijnt niet, maar is dus een comment. 
